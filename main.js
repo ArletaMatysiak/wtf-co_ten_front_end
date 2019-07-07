4395b16e10e8de7c09dbe970f1a6b47dd8cc059b
arli <matysiak.arleta@gmail.com>
2019-07-07 18:00:55 +0200

    innerHTML

--- main.js
+++ main.js
@@ -4,3 +4,15 @@ const age= 27;
 console.log(name);
 console.log(age);
 console.log(`Nazywam się ${name} i mam ${age} lat.`);
+
+const article = document.querySelector('.article--js');
+
+console.log(article.innerHTML);
+
+article.innerHTML = '<strong> "In ac suscipit tellus. Fusce molestie fringilla tellus ac pharetra. Maecenas ultrices id mauris a hendrerit."</strong>';
+
+const paragraphs = document.querySelectorAll('p');
+
+console.log(paragraphs);
+
+console.log(paragraphs[2]);
