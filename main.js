c2e780c40c815d210d9815373518716522ce661b
arli <matysiak.arleta@gmail.com>
2019-07-09 23:35:46 +0200

    cleaning

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log('siema')
 const name = 'arleta';
-const age= 90;
+const age = 90;
 console.log(name);
 console.log(age);
 console.log(`Nazywam się ${name} i mam ${age} lat.`);
@@ -17,43 +17,43 @@ console.log(paragraphs);
 
 console.log(paragraphs[2]);
 
-if ('java script'!= 'java') {
-    console.log ('07.07.19')
+if ('java script' != 'java') {
+    console.log('07.07.19')
 }
 
 if (age < 20) {
-    console.log ('Masz mniej niż 20 lat');
-} else if ((age >= 20) && (age <=30)) {
-    console.log ('Masz pomiędzy 20 a 30 lat');
+    console.log('Masz mniej niż 20 lat');
+} else if ((age >= 20) && (age <= 30)) {
+    console.log('Masz pomiędzy 20 a 30 lat');
 } else {
-    console.log ('Masz więcej niż 30 lat');
+    console.log('Masz więcej niż 30 lat');
 }
 
 switch (age) {
     case 20:
-        console.log (`masz równo 20 lat`);
-    break;
-     case 30:
-        console.log (`masz równo 30 lat`);
-    break;
+        console.log(`masz równo 20 lat`);
+        break;
+    case 30:
+        console.log(`masz równo 30 lat`);
+        break;
     default:
-        console.log (`masz ${age} lat`);
-    break;
+        console.log(`masz ${age} lat`);
+        break;
 }
 
-const amIOld = ( age > 70) ? 'yes' : 'no';
+const amIOld = (age > 70) ? 'yes' : 'no';
 console.log(amIOld);
 
 
-function calculate (x) {
+function calculate(x) {
     x = x + 3;
     console.log(`tradycyjnie: ${x}`);
-    return x*7
+    return x * 7
 }
 
 console.log(calculate(2));
 
-const calculateFat = x => (x+3)*7;
+const calculateFat = x => (x + 3) * 7;
 
 console.log(calculateFat(2));
 
