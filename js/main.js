85c779df77893e74ccf61d11edcef4adf26f3e3a
arli <matysiak.arleta@gmail.com>
2019-07-21 14:19:42 +0200

    feat: welcome js

--- js/main.js
+++ js/main.js
@@ -1,74 +1,9 @@
-const name = 'WTF';
-const week = 5;
-console.log(`Ćwiczę z ${name} i to mój ${week} tydzień.`);
-
-const article = document.querySelector('.article--js');
-
-const paragraphs = document.querySelectorAll('p');
-
-console.log(paragraphs);
-
-console.log(paragraphs[2]);
-
-if ('java script' != 'java') {
-    console.log('07.07.19')
+const welcome = (name, week) => {
+    console.log (`Cześć ${name}, to Twój ${week} tydzień z WTF.`)
 }
 
-if (week > 5) {
-    console.log('ok');
-} else if ((week >= 5) && (week <= 9)) {
-    console.log('elegancko');
-} else {
-    console.log('brawo Ty');
-}
+const myName = 'Arli';
+const myWeek = 6;
 
-switch (week) {
-    case 5:
-        console.log(`ja też, dobrze nam idzie!`);
-        break;
-    case 6 - 8:
-        console.log(`git`);
-        break;
-    case 9:
-        console.log(`gratulacje!`);
-        break;
-    default:
-        console.log(`To Twój ${week} tydzień`);
-        break;
-}
-
-const amIOld = (week > 1) ? 'yes' : 'no';
-console.log(amIOld);
-
-
-function calculate(x) {
-    x = x + 3;
-    console.log(`tradycyjnie: ${x}`);
-    return x * 7
-}
-
-console.log(calculate(2));
-
-const calculateFat = x => (x + 3) * 7;
-
-console.log(calculateFat(2));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+welcome (myName, myWeek); 
 
