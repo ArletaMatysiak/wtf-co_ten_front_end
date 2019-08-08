944f89d82622171ccdb5f4772334a352a8cb8b6c
arli <matysiak.arleta@gmail.com>
2019-08-08 18:16:22 +0200

    feat: new layout flex box + grid

--- js/main.js
+++ js/main.js
@@ -7,3 +7,33 @@ const myWeek = 6;
 
 welcome (myName, myWeek); 
 
+
+
+const button = document.querySelector('.article___button--js');
+console.log(button)
+
+
+button.addEventListener('click', (e) => {
+const header = document.querySelector('.article__title--js');
+header.innerHTML = 'klik, klik';
+alert ('hello');
+});
+
+
+
+function oknoAlert () {
+    alert('w budowie');
+}
+
+
+document.querySelector('.navigation__link--js').addEventListener('click', function () {
+    oknoAlert();
+});
+
+
+
+
+    
+
+
+
